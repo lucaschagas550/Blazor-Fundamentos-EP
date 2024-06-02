0b032149ba981def95263c371144ed847a9b8d79
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2024-06-01 21:40:01 -0300

    feat: prerender

---
+++ BlazorWebApp-EP/wwwroot/JavaScript/JavaScript.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var dropdownToggle = document.querySelector('.dropdown-toggle');
+    var dropdownMenu = document.querySelector('.dropdown-menu');
+
+    // Handler para clicar no dropdown
+    function toggleDropdown(event) {
+        dropdownToggle.classList.toggle('active');
+        event.stopPropagation(); // Impede que o clique se propague para o documento
+    }
+
+    // Handler para clicar fora do dropdown
+    function closeDropdown(event) {
+        if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownToggle.classList.remove('active');
+        }
+    }
+
+    // Adiciona os listeners
+    dropdownToggle.addEventListener('click', toggleDropdown);
+    document.addEventListener('click', closeDropdown);
+});
